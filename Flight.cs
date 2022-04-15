f65bf5d0e45f6d2c1144887538a7316b0cf9f59e
WailGree <szilagyi.roland.dominik@gmail.com>
2022-04-15 13:59:06 +0200

    Add project files.

---
+++ Flight.cs
@@ -0,0 +1,7 @@
+﻿namespace PlaneManager
+{
+    internal class Flight
+    {
+        public int Size { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlaneManager
+{
+    internal static class Program
+    {
+
+        static List<Plane> Planes { get; set; } = new List<Plane>();
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            LoadData();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        private static void LoadData()
+        {
+            var fileName = "data.csv";
+            if (File.Exists(fileName))
+            {
+                foreach (var line in File.ReadAllLines(fileName))
+                {
+                    foreach (var data in line.Split(';'))
+                    {
+                        //data.
+                    }
+                }
+            }
+        }
+    }
+}
